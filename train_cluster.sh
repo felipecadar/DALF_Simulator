5e35e162c5dbe46c09f959f1c700e06f42b892e7
Felipe Cadar <cadar@dcc.ufmg.br>
2024-04-16 14:26:02 +0200

    cluster file

--- train_cluster.sh
+++ train_cluster.sh
@@ -7,10 +7,7 @@ module load cuda/11.3.1
 module load cudnn/8.2.4.15-11.4/gcc/11.2.0/module-acinbtf
 module load pytorch/2.0.0/gpu
 
-# opencv/4.5.4/gcc/11.2.0/module-ukdgqsp
-# pip install torch torchvision
-pip install opencv-python
-
+pip install opencv-python tqdm kornia matplotlib tensorboard scipy
 
 cd /work/icb/fc787762/Github/DALF_Simulator/
 
@@ -25,7 +22,7 @@ weights=$(find $FOLDER -type f -name "model_ts1_*.pth" | sort -n | tail -n 1)
 echo $weights
 
 # weights='/home/cadar/Documents/Github/DALF_Simulator/scratch2/model_ts1_120000.pth'
-sim_data='/work/icb/fc787762/Datasets/train_single_object'
+sim_data='/work/icb/fc787762/Datasets/train_single_object/'
 
 # python train.py -sim --pretrained $FOLDER/model_ts1_160000_final.pth -log $FOLDER -s $FOLDER -m ts-fl
 # python train.py -sim -sdpath $sim_data --pretrained $weights -log $FOLDER -s $FOLDER -m ts-fl 
