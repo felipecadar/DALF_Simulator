5582b40648f52cd75bc13c9bdb9d07963ede2c91
Felipe Cadar <cadar@dcc.ufmg.br>
2024-05-03 12:16:37 +0200

    finetune

--- train_finetune_simulator.sh
+++ train_finetune_simulator.sh
@@ -1,4 +1,14 @@
 
+module load python/3.10/anaconda/2023.03
+module load cuda/11.3.1
+module load cudnn/8.2.4.15-11.4/gcc/11.2.0/module-acinbtf
+module load pytorch/2.0.0/gpu
+
+pip install opencv-python tqdm kornia matplotlib tensorboard scipy
+
+cd /work/icb/fc787762/Github/DALF_Simulator/
+
+
 sim_data='/work/cadar/Datasets/simulation_v2/train_single_obj/'
 PRETRAINED="weights"
 FOLDER="finetune3"
