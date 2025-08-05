064b123676760f677f0dc8c0381be370d031bbdf
Felipe Cadar <cadar@dcc.ufmg.br>
2025-08-05 13:22:45 -0300

    add UV

--- eval/eval_nonrigid.sh
+++ eval/eval_nonrigid.sh
@@ -1,6 +1,10 @@
 #Please put here the path of images and TPS files from nonrigid benchmark
-PATH_IMGS='../../eval_bench/All_PNG'
-PATH_TPS='../../eval_bench/gt_tps'
+EVAL_BENCH="/Users/cadar/Documents/Datasets/eval_bench"
+
+# PATH_IMGS='../../eval_bench/All_PNG'
+# PATH_TPS='../../eval_bench/gt_tps'
+PATH_IMGS=$EVAL_BENCH'/All_PNG'
+PATH_TPS=$EVAL_BENCH'/gt_tps'
 
 #Set working dir to save results. Please change
 working_dir='./nonrigid_eval'
@@ -14,13 +18,13 @@ benchmark_path='./dalf_benchmark.py'
 metrics_path='./plotUnorderedPR.py'
 
 #Those names were used for the ablation study
-ablation='finetune_desurt_original'
+ablation='finetune_simulation'
 # ablation='finetune_desurt'
 
 #Data Path
 # network_path='../weights/model_ts-fl_simulation.pth'
 # network_path='../desurt_0/model_ts-fl_000800.pth'
-network_path='../desurt_0/model_ts-fl_004000.pth'
+network_path='../logs_single_obj/model_ts-fl_006400.pth'
 
 #Original TPS files
 tps_dir_o=$PATH_TPS
@@ -33,7 +37,7 @@ out_path=$working_dir'/out_'$ablation
 
 echo 'copying original gt_tps '$tps_dir_o' to '$tps_dir
 cp -rf $tps_dir_o $tps_dir
-python3 $extract_gt_path -i $PATH_IMGS --tps_dir $tps_dir --dir -m pgdeal --net_path $network_path 
+python3 $extract_gt_path -i $PATH_IMGS --tps_dir $tps_dir --dir -m pgdeal --net_path $network_path --cpu
 python3 $benchmark_path -i $PATH_IMGS -o $out_path --dir --sift --tps_path $tps_dir --net_path $network_path
 
 # #Remove old results cache
